f7117702499e0ed54be3c2dc8d6b6f20d908a739
Avika <avika2875@gmail.com>
2025-04-16 17:25:56 +0530

    update

---
+++ src/dashboard/resume/components/FormSection.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react'
+import PersonalDetails from './forms/PersonalDetails'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, ArrowRight, LayoutGrid } from 'lucide-react'
+
+
+function FormSection() {
+  const [activeFormIndex,setActiveFormIndex]=useState(1);
+  const [enableNext,setEnableNext]=useState(false)
+  return (
+    <div>
+       <div className='flex justify-between items-center'>
+          <Button varient='outline' size='sm' 
+          className="flex gap-2" > <LayoutGrid/> Theme</Button>
+          <div className='flex gap-2'>
+            {activeFormIndex>1
+            &&<Button size='sm' 
+            onClick={()=>setActiveFormIndex(activeFormIndex-1)}
+            > <ArrowLeft/></Button>}
+            <Button 
+            disabled={!enableNext}
+            className='flex gap-2' size='sm'
+            onClick={()=>setActiveFormIndex(activeFormIndex+1)}
+            > Next 
+            <ArrowRight/> </Button>
+          </div>
+       </div>
+      {/* Personal Details*/}
+         {activeFormIndex==1? <PersonalDetails enabledNext={(v)=>setEnableNext(v)}/>
+         :null}
+      {/* Summary*/}
+          
+      {/* Professional Experience*/}
+        
+      {/*Education Details*/}
+         
+      {/* Skills*/}
+          
+    </div>
+  )
+}
+
+export default FormSection
